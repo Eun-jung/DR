73d11891d0214ec727fe91ab86074ea7d1b1c728
ej-lee <ej-lee@snu.ac.kr>
2016-09-12 20:26:10 +0900

    time distribution of usage-NA rows

--- load_data.R
+++ load_data.R
@@ -2,6 +2,9 @@ library(data.table)
 library(bit64)
 library(readr)
 library(zoo)
+library(ggplot2)
+library(timeDate)
+library(scales)
 
 
 raw_15min = fread("data/raw/1462028400000_1473174000000_15min_usages.csv")
@@ -27,8 +30,22 @@ raw_15min$timestamp = as.POSIXct(raw_15min$timestamp/1000, origin="1970-01-01 00
 data_15min <- raw_15min[, ':='(day = as.Date(timestamp, tz="Asia/Seoul"))]
 
 usage_NA <- data_15min[is.na(raw_15min$unitPeriodUsage),]
-usage_NA <- usage_NA[, .(count = nrow(.SD)), by=c("deviceID", "day")]
-summary(usage_NA$count)
+# usage_NA <- usage_NA[, .(count = nrow(.SD)), by=c("deviceID", "day")]
+# summary(usage_NA$count)
+
+#NA time distribution
+time_dist <- usage_NA[, .(count = nrow(.SD)), by=strftime(timestamp, format="%H:%M")]
+xlabel <- time_dist$strftime
+xlabel <- xlabel[order(xlabel)]
+time_dist$strftime <- as.POSIXct(time_dist$strftime, format = "%H:%M", origin="1970-01-01 00:00", tz='Asia/Seoul') 
+
+plot_time_dist <- ggplot(time_dist, aes(x=strftime)) +
+                  geom_point(data = time_dist, aes(y=count)) +
+                  ggtitle("time distribution of NA rows") +
+                  scale_x_datetime(labels = date_format("%H:%M", tz="Asia/Seoul"), breaks = date_breaks("15 min")) +
+                  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+
 
 rownum_of_day <- data_15min[, .(count = nrow(.SD)), by=c("deviceID", "day")]
 # nrow(rownum_of_day[count!=96])/nrow(rownum_of_day)*100 #9.75215%
